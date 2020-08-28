c52988633d02f6ac6d8429e8241b798ccbdc41dd
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-08-28 21:35:37 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-first_number = "Hello world!"
+first_number = 11 
+second_number = 2  
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
